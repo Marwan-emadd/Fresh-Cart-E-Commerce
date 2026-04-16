5492a4d30db8cab5dd28d08b4b814d3ed3db2242
Marwan-emadd <marwanemadd11@gmail.com>
2026-04-16 23:54:09 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Products from "./Components/Products/Products";
 import Categories from "./Components/Categories/Categories";
 import Brands from "./Components/Brands/Brands";
 import Cart from "./Components/Cart/Cart";
-import NotFound from "./Components/NotFound/NotFound";
+import NotFound from "./Components/Notfound/Notfound";
 import { UserContext } from "./Context/UserContext";
 import ProtectedRouting from "./Components/ProtectedRouting/ProtectedRouting";
 import ProductDetails from "./Components/ProductDetails/ProductDetails";
