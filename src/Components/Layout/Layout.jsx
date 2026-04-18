e12fd80ea4afc722f01c6842cda6cabb7d923872
Marwan-emadd <marwanemadd11@gmail.com>
2026-04-18 17:57:09 +0200

    last commit

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
+      <ScrollRestoration />
       <Navbar />
       <Outlet></Outlet>
       <Footer />
